7e8ea5f0c2afeabd25c33410cd723abe35761806
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2025-04-25 17:56:04 -0500

    one more time

--- flow.py
+++ flow.py
@@ -2,8 +2,7 @@ from prefect import flow
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hello again and again, world!")
-    print("debugging")
+    print("Hello again and again and again, world!")
 
 if __name__ == "__main__":
     my_flow()
