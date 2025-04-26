9d1ed632bb103afeb23775b2b424e827570fa4a6
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2025-04-25 22:13:36 -0500

    say goodbye

--- flow.py
+++ flow.py
@@ -2,7 +2,7 @@ from prefect import flow
 
 @flow(log_prints=True)
 def my_flow():
-    print("Hello again and again and again, world!")
+    print("Goodbye, world!")
 
 if __name__ == "__main__":
     my_flow()
